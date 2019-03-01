0451c80895d7953ec9e935eeb3aa4e7331d926c9
CaptainSensible <carcaroth138@gmail.com>
2019-03-01 18:41:07 +0000

    added MySQL database for comments

--- flask_app.py
+++ flask_app.py
@@ -2,9 +2,22 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="<the username from the 'Databases' tab>",
+    password="<the password you set on the 'Databases' tab>",
+    hostname="<the database host address from the 'Databases' tab>",
+    databasename="<the database name you chose, probably yourusername$comments>",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 comments = []
 
