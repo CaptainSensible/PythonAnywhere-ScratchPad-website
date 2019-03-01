ca3a060ea5d3b1e0e93b619c441e6d1bcae9b3ca
CaptainSensible <carcaroth138@gmail.com>
2019-03-01 21:16:02 +0000

    added some MySQL stuff that I dont understand

--- flask_app.py
+++ flask_app.py
@@ -8,16 +8,23 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="<the username from the 'Databases' tab>",
-    password="<the password you set on the 'Databases' tab>",
-    hostname="<the database host address from the 'Databases' tab>",
-    databasename="<the database name you chose, probably yourusername$comments>",
+    username="CaptainSensible",
+    password="Magnus138!",
+    hostname="CaptainSensible.mysql.pythonanywhere-services.com",
+    databasename="CaptainSensible$Comments",
 )
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
 
 comments = []
 
