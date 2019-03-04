bc8d9081e74f1ce1830590c30a70974fdf284c28
CaptainSensible <carcaroth138@gmail.com>
2019-03-04 17:27:11 +0000

    Password log in screen working

--- flask_app.py
+++ flask_app.py
@@ -27,7 +27,8 @@ class Comment(db.Model):
     content = db.Column(db.String(4096))
 
 
-
+# This is the function (tutorial is calling it a view) that controls what
+# comes up when page is first launched.
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method =="GET":
@@ -38,7 +39,24 @@ def index():
     db.session.commit()
     return redirect (url_for("index"))
 
-@app.route("/login/")
+
+
+#This is the function that controls the login screen.
+#"GET", "POST" allow viewing (get) and submitted data (POST) of log in credentials.
+@app.route("/login/", methods=["GET", "POST"])
+
+#Below defines the function "login()" then says if just viewing the page (GET method)
+#show the login screen (also it creates a variable "error" and sets it to false.
 def login():
-    return render_template("login_page.html")
+    if request.method == "GET":
+        return render_template("login_page.html", error=False)
+
+#This says if the system recieves anything other than a "GET" method (i.e. a POST)
+#check the username and password fields, if they dont equal (!=) "admin" and "secret"
+#reload the login screen but set the "error" variable to True.
+    if request.form["username"] != "admin" or request.form["password"] != "secret":
+        return render_template("login_page.html", error=True)
+
+#And this just says if teh username and password are correct, load the app (index.html)
+    return redirect (url_for("index"))
 
